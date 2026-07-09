f3ea018146438d66b3bd2b96d6223f2877c856ee
Loai Wael <loaiwael.dev@gmail.com>
2026-07-09 17:13:05 +0300

    DEL notifications system

--- features/notifications/index.ts
+++
@@ -1,5 +0,0 @@
-// Public API surface for the notifications package
-export * from "./components/NotificationBell";
-export * from "./hooks/use-fcm";
-export * from "./services/firebase-client";
-export * from "./types";
--- features/notifications/types/index.ts
+++
@@ -1,6 +0,0 @@
-export interface Notification {
-  title: string;
-  body: string;
-  icon?: string;
-  data?: Record<string, string>;
-}
